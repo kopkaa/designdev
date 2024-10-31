8e6f799238b2124fd42eaf266b61d4c5af81edcb
Jan Hromádka <jan.hromadka@yditlabs.cz>
2024-10-31 17:06:46 +0100

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import globals from "globals";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+export default [
+
+  {
+    files: ["**/*.ts"],
+		ignores: ["vite.config.ts"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.app.json",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      ...require("@typescript-eslint/eslint-plugin").configs.recommended.rules,
+    },
+  },
+
+
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: require("vue-eslint-parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        parser: require("@typescript-eslint/parser"),
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      vue: require("eslint-plugin-vue"),
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      ...require("eslint-plugin-vue").configs["vue3-recommended"].rules,
+      ...require("@typescript-eslint/eslint-plugin").configs.recommended.rules,
+
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+      "vue/multi-word-component-names": "error",
+      "vue/no-unused-vars": "warn",
+    },
+  },
+
+];
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+console.log('HOJ')
+</script>
+
+<template>
+    <div
+        class="mx-auto container min-h-screen flex flex-col justify-center items-center p-2"
+    >
+        <img alt="Vue logo" src="./assets/vue.svg" />
+				<p v-html="'cc'"></p>
+        <HelloWorld msg="Hello Vue 3 + Vite" />
+    </div>
+</template>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
